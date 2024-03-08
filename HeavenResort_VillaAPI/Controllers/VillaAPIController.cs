5a498d30f2c89be1cf924062796b1e5a1530cff9
Alireza Izadi <alirezaizad@outlook.com>
2024-03-08 17:52:28 +0330

    ✅Added response types to the API endpoints

--- HeavenResort_VillaAPI/Controllers/VillaAPIController.cs
+++ HeavenResort_VillaAPI/Controllers/VillaAPIController.cs
@@ -10,14 +10,33 @@ namespace HeavenResort_VillaAPI.Controllers
     public class VillaAPIController:ControllerBase
     {
         [HttpGet]
-        public IEnumerable<VillaDTO> GetVillas()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return VillaStore.villaList;
+            return Ok(VillaStore.villaList);
         }
-        [HttpGet("{id: int}")]
-        public VillaDTO GetVilla(int id)
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<VillaDTO> GetVilla(int id)
         {
-            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if(id > 0)
+            {
+                var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+                if (villa != null)
+                {
+                    return Ok(villa);
+                }
+                else
+                {
+                    return NotFound("No data!");
+                }
+            }
+            else
+            {
+                return BadRequest("Please provide a valid id");
+            }
         }
     }
 }
