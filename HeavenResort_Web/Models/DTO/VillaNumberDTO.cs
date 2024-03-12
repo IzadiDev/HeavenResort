43129d913f7f5d2eb3bcdb602c13f7fe0c09f6d8
Alireza Izadi <alirezaizad@outlook.com>
2024-03-12 19:43:53 +0330

    ✅Added HeavenResort_Web Project and Utility Class Library to project

---
+++ HeavenResort_Web/Models/DTO/VillaNumberDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace HeavenResort_Web.Models.DTO
+{
+    public class VillaNumberDTO
+    {
+        [Required]
+        public int VillaNo { get; set; }
+        public string SpecialDetails { get; set; }
+    }
+}
